aa1077fc5a49639ae925ff4606aaa8b00fbf371a
gnayuy <gnayuy@gmail.com>
2016-07-25 17:26:48 -0400

    add vert, plane, and surf class to obtain the surface

--- src/surfrecon.cpp
+++ src/surfrecon.cpp
@@ -1,4 +1,4 @@
-// objsplit: to split a 3D object by a given point cloud
+// surfrecon: to reconstruct a surface from a point cloud
 // Yang Yu (gnayuy@gmail.com)
 
 // usage:
@@ -6,28 +6,77 @@
 //
 
 //
-#include "objsplit.h"
+#include "surfrecon.h"
 
 // command line interface
 
-DEFINE_string(obj, "", "object input ");
 DEFINE_string(pc, "", "point cloud input .off");
 DEFINE_string(o, "", "output");
 DEFINE_int64(minx, 0, "bouding box minx");
 DEFINE_int64(miny, 0, "bouding box miny");
 DEFINE_int64(minz, 0, "bouding box minz");
 
+//
+Vert::Vert()
+{
+}
+
+Vert::~Vert()
+{
+}
+
+//
+Plane::Plane()
+{
+}
+
+Plane::~Plane()
+{
+}
+
+//
+Surf::Surf()
+{
+}
+
+Surf::~Surf()
+{
+}
+
+void Surf::getPlanes()
+{
+    Point P,Q,R;
+    Plane plane;
+    
+    //
+    planes.clear();
+    
+    //
+    for(int i=0; i<faces.size(); i++)
+    {
+        P = points[faces[i].p];
+        Q = points[faces[i].q];
+        R = points[faces[i].r];
+        
+        // cross product
+        plane.a = (Q.y()-P.y())*(R.z()-P.z()) - (Q.z()-P.z())*(R.y()-P.y());
+        plane.b = (Q.z()-P.z())*(R.x()-P.x()) - (Q.x()-P.x())*(R.z()-P.z());
+        plane.c = (Q.x()-P.x())*(R.y()-P.y()) - (Q.y()-P.y())*(R.x()-P.x());
+        
+        plane.d = -( plane.a*P.x() + plane.b*P.y() + plane.c*P.z() );
+        
+        planes.push_back(plane);
+    }
+}
 
 // OpenVDB I/O class
 VDBIO::VDBIO()
 {
     openvdb::initialize ();
-    openvdb::Grid<NeuronMaskTree>::registerGrid();
 }
 
 VDBIO::~VDBIO()
 {
-    
 }
 
 void VDBIO::read(char const *filename)
@@ -40,8 +89,8 @@ void VDBIO::read(char const *filename)
         OPENVDB_LOG_WARN(filename << " is empty");
     }
     
-    NeuronMaskGrid::Ptr grid = openvdb::gridPtrCast<NeuronMaskGrid>(baseGrids->at(0));
-    NeuronMaskGrid::Accessor accessor = grid->getAccessor();
+    openvdb::BoolGrid::Ptr grid = openvdb::gridPtrCast<openvdb::BoolGrid>(baseGrids->at(0));
+    openvdb::BoolGrid::Accessor accessor = grid->getAccessor();
     std::cout << "Leaf Nodes: " << grid->tree().leafCount() << std::endl;
 }
 
@@ -56,14 +105,14 @@ void VDBIO::write(char const *filename)
 // main func
 int main(int argc, char *argv[])
 {
-    cout<<"objsplit: to split a 3D object by a given point cloud\n";
+    cout<<"surfrecon: to reconstruct a surface from a point cloud\n";
     
     //
-    gflags::SetUsageMessage("objsplit -obj <object> -pc <pointcloud> -o <output>");
+    gflags::SetUsageMessage("surfrecon -pc <PointCloud> -o <Output>");
     gflags::ParseCommandLineFlags(&argc, &argv, true);
     
     std::cout<<"Parameters:"<<std::endl;
-    std::cout<<"inputs: \n  <Object>: "<<FLAGS_obj<<" \n  <PointCloud>: "<<FLAGS_pc<<" \noutput: \n  <splitObjects>: "<<FLAGS_o<<std::endl;
+    std::cout<<"input: \n  <PointCloud>: "<<FLAGS_pc<<" \noutput: \n  <Output>: "<<FLAGS_o<<std::endl;
     
     
     //
@@ -71,78 +120,24 @@ int main(int argc, char *argv[])
     double by = FLAGS_miny;
     double bz = FLAGS_minz;
     
-//    ApproxMVBB::Vector3 origin = ApproxMVBB::Vector3(bx, by, bz);
-//    
-//    cout<<"origin: "<<origin.transpose()<<endl;
-//    
-//
-    // 3d object
-    VDBIO vdb;
-    vdb.read(const_cast<char*>(FLAGS_obj.c_str()));
+    //
+    Surf surface;
     
-    // points
-    Point_collection points;
+    //
     std::ifstream in(FLAGS_pc);
     std::cerr << "Reading " << std::flush;
-    if( !in || !CGAL::read_off_points( in, std::back_inserter( points ) ) ) {
+    if( !in || !CGAL::read_off_points( in, std::back_inserter( surface.points ) ) ) {
         std::cerr << "Error: cannot read file" << std::endl;
         return -1;
     }
     
-    std::cerr << "done: " << points.size() << " points." << std::endl;
+    std::cerr << "done: " << surface.points.size() << " points." << std::endl;
     
 //    for(int i; i<points.size(); i++)
 //    {
 //        cout<<" "<<points[i].x()<<" "<<points[i].y()<<" "<<points[i].z()<<endl;
 //    }
-    
-    // obb
-//    ApproxMVBB::Matrix3Dyn pointSet(3,points.size());
-    
-//    for(int i=0; i<points.size(); i++)
-//    {
-//        pointSet.col(i) = ApproxMVBB::Vector3(points[i].x(),points[i].y(),points[i].z()) - origin;
-//        
-//        cout<<" "<<pointSet.col(i).transpose()<<endl;
-//    }
-    
 
-//    ApproxMVBB::Matrix3Dyn pointSet(3,3);
-//    
-//    pointSet.col(0) = ApproxMVBB::Vector3(25, 25, 25);
-//    pointSet.col(1) = ApproxMVBB::Vector3(25, 26, 25);
-//    pointSet.col(2) = ApproxMVBB::Vector3(26, 25, 26);
-//    
-//    ApproxMVBB::OOBB oobb = ApproxMVBB::approximateMVBB(pointSet,0.001,500,5,0,5);
-//    
-//    std::cout << "Computed OOBB: " << std::endl
-//    << "---> lower point in OOBB frame: " << oobb.m_minPoint.transpose() << std::endl
-//    << "---> upper point in OOBB frame: " << oobb.m_maxPoint.transpose() << std::endl
-//    << "---> coordinate transformation A_IK matrix from OOBB frame K to world frame I" << std::endl
-//    << oobb.m_q_KI.matrix() << std::endl
-//    << "---> this is also the rotation matrix R_KI  which turns the world frame I into the OOBB frame K" <<std::endl << std::endl;
-//    
-//    
-//    // To make all points inside the OOBB :
-//    ApproxMVBB::Matrix33 A_KI = oobb.m_q_KI.matrix().transpose(); // faster to store the transformation matrix first
-//    auto size = pointSet.cols();
-//    for( unsigned int i=0;  i<size; ++i ) {
-//        oobb.unite(A_KI*pointSet.col(i));
-//    }
-//    std::cout << "OOBB with all point included: " << std::endl
-//    << "---> lower point in OOBB frame: " << oobb.m_minPoint.transpose() << std::endl
-//    << "---> upper point in OOBB frame: " << oobb.m_maxPoint.transpose() << std::endl;
-//    
-//
-//    ApproxMVBB::Vector3 p = oobb.m_q_KI * oobb.m_minPoint;
-//    ApproxMVBB::Vector3 q = oobb.m_q_KI * oobb.m_maxPoint;
-//    
-//    cout<<"lower point: "<<p.transpose()<<"; upper point: "<<q.transpose()<<endl;
-    
-    
-    
-    
-    
     
     Timer t;
     t.start();
@@ -150,7 +145,7 @@ int main(int argc, char *argv[])
     // Construct the mesh in a scale space.
     Reconstruction reconstruct( 10, 200 );
     
-    reconstruct.reconstruct_surface( points.begin(), points.end(), 4,
+    reconstruct.reconstruct_surface( surface.points.begin(), surface.points.end(), 4,
                                     false, // Do not separate shells
                                     true // Force manifold output
                                     );
@@ -171,14 +166,14 @@ int main(int argc, char *argv[])
     
     
     
-    t.reset();
-    std::ofstream out(FLAGS_o);
-    for( Triple_iterator it = reconstruct.surface_begin( ); it != reconstruct.surface_end(  ); ++it )
-        out << "3 "<< *it << '\n'; // We write a '3' in front so that it can be assembled into an OFF file
-    
-    std::cerr << "Writing result in " << t.time() << " sec." << std::endl;
-    
-    out.close();
+//    t.reset();
+//    std::ofstream out(FLAGS_o);
+//    for( Triple_iterator it = reconstruct.surface_begin( ); it != reconstruct.surface_end(  ); ++it )
+//        out << "3 "<< *it << '\n'; // We write a '3' in front so that it can be assembled into an OFF file
+//    
+//    std::cerr << "Writing result in " << t.time() << " sec." << std::endl;
+//    
+//    out.close();
     
 //    t.reset();
 //    std::ofstream garbage ("garbage.off");
@@ -189,6 +184,33 @@ int main(int argc, char *argv[])
 //    
 //    garbage.close ();
     
+    
+    //
+    VDBIO vdb;
+    
+    openvdb::FloatGrid::Ptr grid = openvdb::FloatGrid::create();
+    openvdb::FloatGrid::Accessor accessor = grid->getAccessor();
+    
+    grid->setTransform (openvdb::math::Transform::createLinearTransform (0.5));
+    grid->setGridClass (openvdb::GRID_FOG_VOLUME);
+    grid->setName ("surface");
+    
+    openvdb::Coord ijk;
+    
+    ijk[0] = 100; // x
+    ijk[1] = 100; // y
+    ijk[2] = 100; // z
+    
+    accessor.setValue(ijk, 1.0);
+    
+    vdb.grids.push_back(grid);
+    
+    vdb.write(const_cast<char*>(FLAGS_o.c_str()));
+    
+    
+    
+    
+    //
     std::cerr << "Done." << std::endl;
 
     
--- src/surfrecon.h
+++ src/surfrecon.h
@@ -1,9 +1,9 @@
-// objsplit: to split a 3D object by a given point cloud
+// surfrecon: to reconstruct a surface from a point cloud
 // Yang Yu (gnayuy@gmail.com)
 
 
-#ifndef __OBJSPLIT_H__
-#define __OBJSPLIT_H__
+#ifndef __SURFRECON_H__
+#define __SURFRECON_H__
 
 //
 #include <gflags/gflags.h>
@@ -12,8 +12,6 @@
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/IO/read_off_points.h>
 #include <CGAL/Timer.h>
-
-//#include <ApproxMVBB/ComputeApproxMVBB.hpp>
 
 #include <openvdb/openvdb.h>
 #include <openvdb/Types.h>
@@ -62,7 +60,7 @@ typedef CGAL::Exact_predicates_inexact_constructions_kernel     Kernel;
 typedef CGAL::Scale_space_surface_reconstruction_3< Kernel >    Reconstruction;
 
 typedef Reconstruction::Point                                   Point;
-typedef std::vector< Point >                                    Point_collection;
+typedef std::vector< Point >                                    PointSet;
 
 typedef Reconstruction::Triple_const_iterator                   Triple_iterator;
 typedef CGAL::Timer Timer;
@@ -70,8 +68,47 @@ typedef CGAL::Timer Timer;
 
 // classes
 
-typedef openvdb::tree::Tree5<bool, 5, 2, 2, 3>::Type  NeuronMaskTree;
-typedef openvdb::Grid<NeuronMaskTree>                 NeuronMaskGrid;
+// Vert
+class Vert
+{
+public:
+    Vert();
+    ~Vert();
+    
+public:
+    int p, q, r;
+};
+
+typedef std::vector<Vert>    FaceSet;
+
+// Plane
+class Plane
+{
+public:
+    Plane();
+    ~Plane();
+    
+public:
+    double a,b,c,d;
+};
+
+typedef std::vector<Plane>    PlaneSet;
+
+// Surf
+class Surf
+{
+public:
+    Surf();
+    ~Surf();
+    
+public:
+    void getPlanes();
+    
+public:
+    PointSet points;
+    FaceSet faces;
+    PlaneSet planes;
+};
 
 // OpenVDB I/O
 class VDBIO
@@ -90,4 +127,4 @@ public:
 };
 
 
-#endif // __OBJSPLIT_H__
+#endif // __SURFRECON_H__
