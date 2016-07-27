90ecd0b0e216e503352c2dac0776e4f7545692f3
gnayuy <gnayuy@gmail.com>
2016-07-27 16:42:53 -0400

    basically incorporate Zlatanova's 3D raster engine into surfrecon

--- src/surfrecon.h
+++ src/surfrecon.h
@@ -1,4 +1,4 @@
-// surfrecon: to reconstruct a surface from a point cloud
+// surfrecon: to reconstruct a surface from a point cloud and voxelization
 // Yang Yu (gnayuy@gmail.com)
 
 
@@ -6,19 +6,11 @@
 #define __SURFRECON_H__
 
 //
-#include <gflags/gflags.h>
-
-#include <CGAL/Scale_space_surface_reconstruction_3.h>
-#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-#include <CGAL/IO/read_off_points.h>
-#include <CGAL/Timer.h>
-
-#include <glm/vec3.hpp>
-
 #include <deque>
 #include <queue>
 #include <cstdlib>
 #include <cstdio>
+#include <iostream>
 #include <fstream>
 #include <sstream>
 #include <cstring>
@@ -29,14 +21,18 @@
 #include <ctime>
 #include <limits>
 #include <complex>
-#include <iostream>
-
+#include <float.h>
+#include <glm/vec3.hpp>
 #include <boost/foreach.hpp>
 
-// c++11
-//#include <thread>
-//#include <mutex>
-//#include <future>
+#ifdef Use_OpenMP
+#include <omp.h>
+#endif
+
+#include <CGAL/Scale_space_surface_reconstruction_3.h>
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/IO/read_off_points.h>
+#include <CGAL/Timer.h>
 
 //
 using namespace std;
@@ -56,6 +52,24 @@ typedef glm::vec3                                               VoxelType;
 typedef std::vector< VoxelType >                                VoxelSet;
 
 // classes
+
+//
+typedef struct {
+    double x, y, z;
+} Vertex;
+
+typedef struct {
+    Vertex *p1, *p2, *p3;
+} Face;
+
+typedef struct {
+    unsigned int x, y, z;
+} Voxel;
+
+typedef struct {
+    Vertex start, end;
+} LineSegment;
+
 
 // Vert
 class Vert
@@ -95,6 +109,33 @@ public:
     void getPlanes();
     void getSurfaceInVoxels(VoxelSet &voxels, float thresh);
     void surfrecon(VoxelSet pcIn, VoxelSet &pcOut);
+    
+public:
+    // Voxelization with Zlatanova's 3D raster engine (http://www.sciencedirect.com/science/article/pii/S2215016116000029)
+    // Zlatanova, Sisi, et al. "Towards 3D raster GIS: On developing a raster engine for spatial DBMS."
+    // ISPRS WG IV/2 Workshop "Global Geospatial Information and High Resolution Global Land Cover/Land Use Mapping"
+    // Novosibirsk, Russian Federation, 21 April 2016.
+    int voxelizeMesh(VoxelSet &voxels, Vertex *vertices, unsigned int nVertices, Face **mesh, unsigned int nTriangles, Vertex *vSize, int co, int num_threads);
+    
+    void assign(Vertex *to, Vertex *from);
+    void addTo(Vertex *result, Vertex *v, double x, double y, double z);
+    void subtractFrom(Vertex *result, Vertex *v, double x, double y, double z);
+    void add(Vertex *result, Vertex *v1, Vertex *v2);
+    void subtract(Vertex *result, Vertex *v1, Vertex *v2);
+    void crossProduct(Vertex *result, Vertex *v1, Vertex *v2);
+    double dotProduct(Vertex *v1, Vertex *v2);
+    void product(Vertex *result, double s, Vertex *v);
+    double distance(Vertex *v1, Vertex *v2);
+    void getRBoundingBox(Vertex *vertices, unsigned int nVertices, Vertex *min, Vertex *max);
+    void centerOfVoxel_(int x, int y, int z, Vertex *rMin, Vertex *vSize, Vertex *voxelCenter);
+    void centerOfVoxel(Voxel *voxel, Vertex *rMin, Vertex *vSize, Vertex *voxelCenter);
+    double distancePointSegment(LineSegment *l, Vertex *p);
+    double distancePointTriangle(Face *f, Vertex *p);
+    int isNear(int num_threads, Face **mesh, unsigned int nTriangles, Vertex *voxelCenter, Vertex *vSize);
+    int intersectsTriangleLine(Face *f, LineSegment *l);
+    int intersectsMeshLine(Face **mesh, unsigned int nTriangles, LineSegment *l);
+    int intersectsMesh26(Face **mesh, unsigned int nTriangles, Vertex *voxelCenter, Vertex *vSize);
+    int intersectsMesh6(Face **mesh, unsigned int nTriangles, Vertex *voxelCenter, Vertex *vSize);
     
 public:
     PointSet points;
---
+++ test/surfvol.cpp
@@ -0,0 +1,119 @@
+// surfvol: to test surfrecon library with test data
+// Yang Yu (gnayuy@gmail.com)
+
+//
+#include <gflags/gflags.h>
+#include "surfrecon.h"
+
+// command line interface
+DEFINE_string(pc, "", "PointCloud <.off>");
+DEFINE_string(o, "", "Voxels <.swc>");
+DEFINE_double(t, 1.0, "threshold");
+
+// main func
+int main(int argc, char *argv[])
+{
+    cout<<"surfvol: to reconstruct a surface from a point cloud and voxelize it.\n";
+    
+    //
+    gflags::SetUsageMessage("surfvol -pc <PointCloud> -o <Voxels>");
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+    
+    std::cout<<"Parameters:"<<std::endl;
+    std::cout<<"  <PointCloud>: "<<FLAGS_pc<<" \n  <Voxels>: "<<FLAGS_o<<std::endl;
+    
+    
+    //
+    Surf surface;
+    
+    // Threshold
+    float thresh = float(FLAGS_t);
+    
+    //
+    std::ifstream in(FLAGS_pc);
+    std::cerr << "Reading " << std::flush;
+    if( !in || !CGAL::read_off_points( in, std::back_inserter( surface.points ) ) ) {
+        std::cerr << "Error: cannot read file" << std::endl;
+        return -1;
+    }
+    
+    std::cerr << "done: " << surface.points.size() << " points." << std::endl;
+    
+    //
+    Timer t;
+    t.start();
+    
+    // Construct the mesh in a scale space.
+    Reconstruction reconstruct( 10, 200 );
+    
+    reconstruct.reconstruct_surface( surface.points.begin(), surface.points.end(), 4,
+                                    false, // Do not separate shells
+                                    true // Force manifold output
+                                    );
+    
+    std::cerr << "Reconstruction done in " << t.time() << " sec." << std::endl;
+    
+    //
+    surface.faces.clear();
+    for( Triple_iterator it = reconstruct.surface_begin( ); it != reconstruct.surface_end(  ); ++it )
+    {
+        //cout << "3 "<< *it << '\n';
+        
+        int c=0;
+        Vert v;
+        for (auto i:*it)
+        {
+            //std::cout << ' ' << points[i].x();
+            
+            switch (c++)
+            {
+                case 0:
+                    v.p = i;
+                    break;
+                    
+                case 1:
+                    v.q = i;
+                    break;
+                    
+                case 2:
+                    v.r = i;
+                    break;
+                    
+                default:
+                    break;
+            }
+        }
+        //cout<<endl;
+        
+        surface.faces.push_back(v);
+    }
+    
+    t.reset();
+    surface.getPlanes();
+    
+    cout<<"faces: "<<surface.faces.size()<<" planes: "<<surface.planes.size()<<" in "<< t.time() << " sec." <<endl;
+    
+    
+    t.reset();
+    VoxelSet pcOut;
+    surface.getSurfaceInVoxels(pcOut, thresh);
+    cout<<"voxels: "<<pcOut.size()<< " in "<<t.time()<<" sec."<<endl;
+    
+    // Output
+    t.reset();
+    std::ofstream out(FLAGS_o);
+    for(long i=0; i<pcOut.size(); i++)
+    {
+        out << pcOut[i][0] <<" "<< pcOut[i][1] <<" "<<pcOut[i][2]<< '\n';
+    }
+    std::cerr << "Writing "<<pcOut.size()<<" voxels in " << t.time() << " sec." << std::endl;
+    
+    out.close();
+    
+    //
+    std::cerr << "Done." << std::endl;
+    
+    
+    //
+    return 0;
+}
